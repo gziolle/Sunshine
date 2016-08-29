17add8b516f496cb752d64106ccc6021f2280c5a
gziolle <gziolle@gmail.com>
2016-08-29 19:58:13 -0300

    Chapter 1
    Changes made to complete the first chapter

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.android.sunshine.app;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +62,20 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> forecastData = new ArrayList<>();
+            forecastData.add("String 1");
+            forecastData.add("String 2");
+            forecastData.add("String 3");
+            forecastData.add("String 4");
+            forecastData.add("String 5");
+            forecastData.add("String 6");
+            forecastData.add("String 7");
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textView, forecastData);
+            ListView listView = (ListView)rootView.findViewById(R.id.listView_forecast);
+            listView.setAdapter(adapter);
+
             return rootView;
         }
     }
