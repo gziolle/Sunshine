63279a83a4ec6bfb816230b1e3dddac48960f94b
Guilherme Ziolle <gziolle@gmail.com>
2016-09-22 16:07:43 -0300

    Fixed DB issues Added a Content Provider to query weather data from the DB.

--- app/src/main/java/com/example/android/sunshine/app/database/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/database/WeatherContract.java
@@ -1,5 +1,8 @@
 package com.example.android.sunshine.app.database;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 import android.text.format.Time;
 
@@ -7,6 +10,13 @@ import android.text.format.Time;
  * Created by gziolle on 9/8/2016.
  */
 public final class WeatherContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_LOCATION = "location";
+    public static final String PATH_WEATHER = "weather";
 
     // To make it easy to query for the exact date, we normalize all dates that go into
     // the database to the start of the the Julian day at UTC.
@@ -31,6 +41,16 @@ public final class WeatherContract {
         public static final String COLUMN_COORD_LAT = "coord_lat";
         /*The longitude coordinate in degrees*/
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
     /*Inner class that defines the table contents of the weather table*/
@@ -62,6 +82,48 @@ public final class WeatherContract {
         public static final String COLUMN_WIND_SPEED = "wind_speed";
         //Degrees are meteorological degrees (e.g. 0 is north and 100 is south). Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(
+                String locationSetting, long startDate) {
+            long normalizedDate = normalizeDate(startDate);
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATE, Long.toString(normalizedDate)).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSetting, long date) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendPath(Long.toString(normalizeDate(date))).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static long getDateFromUri(Uri uri) {
+            return Long.parseLong(uri.getPathSegments().get(2));
+        }
+
+        public static long getStartDateFromUri(Uri uri) {
+            String dateString = uri.getQueryParameter(COLUMN_DATE);
+            if (null != dateString && dateString.length() > 0)
+                return Long.parseLong(dateString);
+            else
+                return 0;
+        }
 
     }
 }
--- app/src/main/java/com/example/android/sunshine/app/database/WeatherDBHelper.java
+++ app/src/main/java/com/example/android/sunshine/app/database/WeatherDBHelper.java
@@ -11,7 +11,7 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
 
     //Represents the database version. If new changes are applied to the database or its tables
     //This number must be increased to trigger the onUpgrade method.
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
     //Database name that will be used in the file system.
     public static final String DATABASE_NAME = "weather.db";
@@ -45,10 +45,10 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
                     WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
                     WeatherContract.WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
                     //Added the Location ID column as a foreign key
-                    "FOREIGN KEY (" + WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " + WeatherContract.LocationEntry._ID + "(" + WeatherContract.LocationEntry._ID + ")," +
+                    "FOREIGN KEY (" + WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " + WeatherContract.LocationEntry.TABLE_NAME + "(" + WeatherContract.LocationEntry._ID + ")," +
                     //Added a constraint to have only one weather entry per date per location. In case a new entry for the same date and
                     // location is inserted into the table, it will replace the old entry.
-                    " UNIQUE (" + WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ", " + WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
+                    " UNIQUE (" + WeatherContract.WeatherEntry.COLUMN_DATE + ", " + WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
     public WeatherDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -66,8 +66,8 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
         //Since the tables are used as a cache from a web service instead of
         //user-generated data, we'll drop the tables and create them again.
         //If the tables are filled by user data, you should use the ALTER TABLE command instead.
-        db.execSQL("DROP TABLE IF EXISTS " + WeatherContract.WeatherEntry.TABLE_NAME + ");");
-        db.execSQL("DROP TABLE IF EXISTS " + WeatherContract.LocationEntry.TABLE_NAME + ");");
+        db.execSQL("DROP TABLE IF EXISTS " + WeatherContract.WeatherEntry.TABLE_NAME + ";");
+        db.execSQL("DROP TABLE IF EXISTS " + WeatherContract.LocationEntry.TABLE_NAME + ";");
         onCreate(db);
     }
 }
