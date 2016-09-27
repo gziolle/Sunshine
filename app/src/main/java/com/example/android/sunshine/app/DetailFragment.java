516a9db9f0e2bc06fa527df00580759e72a055e0
Guilherme Ziolle <gziolle@gmail.com>
2016-09-27 16:35:04 -0300

    - Added a new list item  - Added a today's list item.

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -127,8 +127,8 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
 
         boolean isMetric = Utility.isMetric(getActivity());
 
-        String high = Utility.formatTemperature(data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
-        String low = Utility.formatTemperature(data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+        String high = Utility.formatTemperature(getActivity(), data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+        String low = Utility.formatTemperature(getActivity(), data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
 
         mForecastData = String.format("%s - %s - %s/%s", dateStr, description, high, low);
 
--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -14,6 +15,8 @@ import android.widget.TextView;
 public class ForecastAdapter extends CursorAdapter {
 
     private static Context mContext;
+    private static final int TODAY_VIEW_ITEM = 0;
+    private static final int FUTURE_VIEW_LIST_ITEM = 1;
 
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -22,24 +25,65 @@ public class ForecastAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        switch (viewType) {
+            case TODAY_VIEW_ITEM:
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            case FUTURE_VIEW_LIST_ITEM:
+                layoutId = R.layout.list_item_forecast;
+                break;
+        }
+
+        View view = LayoutInflater.from(context).inflate(layoutId, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
         return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView tv = (TextView) view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+
+        ViewHolder holder = (ViewHolder) view.getTag();
+
+        holder.icon.setImageResource(R.drawable.ic_launcher);
+
+        long dateInMilli = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+
+        holder.dateTextView.setText(Utility.getFriendlyDayString(mContext, dateInMilli));
+
+        holder.forecastTextView.setText(cursor.getString(ForecastFragment.COL_WEATHER_DESC));
+
+        boolean isMetric = Utility.isMetric(mContext);
+
+        double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        holder.highTextView.setText(Utility.formatTemperature(mContext, high, isMetric));
+
+        double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+        holder.lowTextView.setText(Utility.formatTemperature(mContext, low, isMetric));
     }
 
+    @Override
+    public int getViewTypeCount() {
+        return 2; // Today or Future list items.
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? TODAY_VIEW_ITEM : FUTURE_VIEW_LIST_ITEM;
+    }
 
     /**
      * Prepare the weather high/lows for presentation.
      */
     private String formatHighLows(double high, double low) {
-        boolean isMetric = Utility.isMetric(mContext);
-        String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
-        return highLowStr;
+//        boolean isMetric = Utility.isMetric(mContext);
+//        String highLowStr = Utility.formatTemperature(contehigh, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
+//        return highLowStr;
+        return ";";
     }
 
     /*
@@ -55,5 +99,23 @@ public class ForecastAdapter extends CursorAdapter {
         return Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE)) +
                 " - " + cursor.getString(ForecastFragment.COL_WEATHER_DESC) +
                 " - " + highAndLow;
+    }
+
+    public static class ViewHolder {
+        ImageView icon;
+        TextView dateTextView;
+        TextView forecastTextView;
+        TextView highTextView;
+        TextView lowTextView;
+
+        public ViewHolder(View view) {
+            icon = (ImageView) view.findViewById(R.id.list_item_icon);
+            dateTextView = (TextView) view.findViewById(R.id.list_item_date_textview);
+            forecastTextView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+            highTextView = (TextView) view.findViewById(R.id.list_item_high_textview);
+            lowTextView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        }
+
+
     }
 }
